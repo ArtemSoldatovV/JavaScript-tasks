4000586aa6d6b0c92517c7dea3a0882780c0634d
ArtemSoldatovV <228552266@mail.ru>
2025-03-09 20:54:09 +0300

    main add kt1

---
+++ kt1/src/segment_text/segment_text.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../App.css';
+
+function segment_text({ title, paragraph }) {
+  return (
+	<div class="segment_text">{title} <br/>{paragraph}</div>
+  );
+}
+export default segment_text;
